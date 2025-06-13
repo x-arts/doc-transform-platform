3e675ab5b5616690659c1602505248a838499dcb
xuewenke <379435450@qq.com>
2025-06-13 12:56:28 +0800

    文件转换，添加信息返回的接口

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ import doc_convert
 
 from app.common.response import ResponseHandler
 
+from app import file_store
+from app.view.miner_u_result_file import MinerUResultFile
+
 app = FastAPI()
 
 # 用于存储文件ID和文件名的映射
@@ -95,5 +98,57 @@ async def file_transform(file: UploadFile = File(...)):
         return ResponseHandler.error(str(e))
 
 
+
+@app.post("/api/file-transform/create/info")
+async def file_transform_info(file: UploadFile = File(...)):
+    try:
+        # 生成唯一的文件ID
+        file_id = str(uuid.uuid4())
+
+        # 获取文件后缀名
+        _, file_extension = os.path.splitext(file.filename)
+
+        # 创建文件ID对应的目录
+        file_dir = os.path.join(UPLOAD_DIR, file_id)
+        os.makedirs(file_dir, exist_ok=True)
+
+        # 构建文件保存路径（使用原始文件名）
+        file_location = os.path.join(file_dir, file.filename)
+
+        # 保存文件
+        with open(file_location, "wb+") as file_object:
+            shutil.copyfileobj(file.file, file_object)
+
+        # 调用文档转换服务
+        output_dir = doc_convert.local_file_convert(file_location, file_id)
+
+        # 在输出目录中查找 markdown 文件
+
+        minerURes =  MinerUResultFile()
+
+        for file_name in os.listdir(output_dir):
+            if file_name.endswith('.md'):
+                md_file_path = os.path.join(output_dir, file_name)
+                markdown_file_id =  file_store.upload_file(md_file_path)
+                minerURes.md_file_id = markdown_file_id
+            if file_name.endswith('middle.json'):
+                middle_file_path = os.path.join(output_dir, file_name)
+                middle_file_id = file_store.upload_file(middle_file_path)
+                minerURes.middle_file_id = middle_file_id
+
+            if file_name.endswith('content_list.json'):
+                content_list_file_path = os.path.join(output_dir, file_name)
+                content_list_file_id = file_store.upload_file(content_list_file_path)
+                minerURes.content_list_file_id = content_list_file_id
+
+        return ResponseHandler.success(minerURes)
+
+    except Exception as e:
+        print(f"处理失败: {str(e)}")
+        return ResponseHandler.error(str(e))
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8008)
+
+
---
+++ app/view/miner_u_result_file.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class MinerUResultFile:
+    md_file_id: str
+    middle_file_id: str
+    content_list_file_id: str  # 可以设置默认值
